03e89371be3f919656d702cdcb9c9ad3c0ca7eb7
Max_P <puffetmaximilien21@gmail.com>
2025-04-15 11:12:07 +0200

    new island

--- Assets/Script/RotateCube.cs
+++ Assets/Script/RotateCube.cs
@@ -3,10 +3,17 @@ using UnityEngine;
 public class RotateCube : MonoBehaviour
 {
     [SerializeField] private float _speedRotation;
+    [SerializeField] private float _speedScale;
     [SerializeField] Vector3 _rotationAxis;
 
     void Update()
     {
         transform.Rotate(_rotationAxis * _speedRotation * Time.deltaTime);
     }
+
+    public float SineAmount()
+    {
+        return Mathf.Sin(_speedScale * Time.time);
+    }
+
 }
